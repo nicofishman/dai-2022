748569beb7ded84d29ecb0e54ee4ad96eb8b2e29
nicofishman <nicourifishman@gmail.com>
2022-04-19 09:03:57 -0300

    [DAI] - TP3 Added stacktrace

--- TP3 - Pizzas.API v3/Utils/CustomLog.cs
+++ TP3 - Pizzas.API v3/Utils/CustomLog.cs
@@ -10,35 +10,55 @@ using Newtonsoft.Json;
 
 namespace Pizzas.API.Utils
 {
-    public class CustomLog {
-        private static void CreateLogFolder(){
+    public class CustomLog
+    {
+        private static void CreateLogFolder()
+        {
             string path = ConfigurationHelper.GetConfiguration()["CustomLog:LogFolder"];
-            if(!Directory.Exists(path)){
+            if (!Directory.Exists(path))
+            {
                 Directory.CreateDirectory(path);
             }
         }
-        public static void LogError(string errorData){
-            LogError(errorData, null, null, null);
+        public static void LogError(string errorData)
+        {
+            LogError(errorData, null, null, null, null);
         }
-        public static void LogError(Exception ex){
-            LogError(ex.Message, null, null, null);
-        }
-
-        public static void LogError(string errorData, string errorMessage){
-            LogError(errorData, errorMessage, null, null);
-        }
-        public static void LogError(Exception ex, string className, string functionName){
-            LogError(ex.Message, className, functionName, null);
+        public static void LogError(Exception ex)
+        {
+            LogError(ex.Message, null, null, null, ex.StackTrace);
         }
 
-        public static void LogError(Exception ex, string className, string contexto, object datos){
-            LogError(ex.Message, className, contexto, datos);
+        public static void LogError(string errorData, string errorMessage)
+        {
+            LogError(errorData, errorMessage, null, null, null);
+        }
+        public static void LogError(Exception ex, string className, string functionName)
+        {
+            LogError(ex.Message, className, functionName, null, ex.StackTrace);
         }
 
-        public static void LogError (string errorData, string className, string contexto, object datos){
-            string customData, datosString = "" ;
-            if (datos != null){
+        public static void LogError(Exception ex, string className, string contexto, object datos)
+        {
+            LogError(ex.Message, className, contexto, datos, ex.StackTrace);
+        }
+
+        public static void LogError(string errorData, string className, string contexto, object datos, string stackTrace)
+        {
+            string customData, datosString = "";
+            if (datos != null)
+            {
                 datosString = JsonConvert.SerializeObject(datos);
+            }
+            string[] newStackTrace = stackTrace.Split('\n');
+            string pizzaStackTrace = "";
+            foreach (var item in newStackTrace)
+            {
+                if (!item.TrimStart().StartsWith("at System."))
+                {
+                    Console.WriteLine(item.TrimStart());
+                    pizzaStackTrace += item + "\n";
+                }
             }
 
             customData = string.Format("{0} {1}{2,20}{3,10}{4}",
@@ -46,16 +66,19 @@ namespace Pizzas.API.Utils
                 errorData,
                 (className != null) ? $"\n\tFile\t= {className}" : "",
                 (contexto != null) ? $"\n\tContexto\t= {contexto}" : "",
+                (stackTrace != null) ? $"\n\tStack\t = {pizzaStackTrace}" : "",
                 (datos != null) ? $"\n\tDatos\t= {datos}" : ""
             );
-            try{
+            try
+            {
                 CreateLogFolder();
                 string fullFileName = ConfigurationHelper.GetConfiguration()["CustomLog:LogFolder"] + @"\log.txt";
                 IOHelper.AppendInFile(fullFileName, customData);
             }
-            catch(Exception e){
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
             }
-        } 
+        }
     }
 }
