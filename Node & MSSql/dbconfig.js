8c4976c52f5d2d4ab61d14daf3e7284dbc1afcfc
nicofishman <74215078+nicofishman@users.noreply.github.com>
2022-05-23 10:37:47 -0300

    Process.env

--- Node & MSSql/dbconfig.js
+++ Node & MSSql/dbconfig.js
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 const config = {
-    user: 'Pizzas',
-    password: 'Pizzas',
-    server: 'localhost',
-    database: 'DAI-Pizzas',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     options: {
         trustServerCertificate: true,
         trustedConnection: true
