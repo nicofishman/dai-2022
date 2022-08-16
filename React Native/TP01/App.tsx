3393e166948384c243421a0a8247d82bd94d6603
nicofishman <nicourifishman@gmail.com>
2022-08-16 08:11:15 -0300

    char counter

--- React Native/TP01/App.tsx
+++ React Native/TP01/App.tsx
@@ -1,33 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { TailwindProvider, useTailwind } from 'tailwind-rn';
-
-import utilities from './tailwind.json';
 
 export default function App () {
     const [texto, setTexto] = useState('');
     const [textoMostrar, setTextoMostrar] = useState('');
 
-    const tailwind = useTailwind();
-
     return (
-        <TailwindProvider utilities={utilities}>
-            <View style={tailwind('bg-black w-full h-screen justify-center items-center')}>
-                <View style={tailwind('w-12 items-center justify-center p-10 bg-red-300')}>
-                    <TextInput placeholder="Que onda bro" style={styles.input} onChangeText={(e) => setTexto(e)} />
-                    <TouchableOpacity style={styles.button} onPress={() => setTextoMostrar(texto)}>
-                        <Text style={{ color: 'white', textTransform: 'uppercase' }}>Presioname bb</Text>
-                    </TouchableOpacity>
-                    <Text>{textoMostrar}</Text>
-                </View>
-                <StatusBar style="auto" />
+        <View style={styles.container}>
+            <View style={styles.box}>
+                <TextInput placeholder="Que onda bro" style={styles.input} onChangeText={(e) => setTexto(e)} />
+                <TouchableOpacity style={styles.button} onPress={() => setTextoMostrar(texto)}>
+                    <Text style={{ color: 'white', textTransform: 'uppercase' }}>Presioname bb</Text>
+                </TouchableOpacity>
+                <Text style={styles.counter}>Caracteres: {texto.length}</Text>
+                <Text style={{ color: 'white', fontWeight: '700' }}>{textoMostrar}</Text>
             </View>
-        </TailwindProvider>
+            <StatusBar style="auto" />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    counter: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 20,
+        width: '100%',
+        paddingHorizontal: 30,
+        textAlign: 'right'
+    },
     input: {
         borderWidth: 1,
         borderColor: '#000',
